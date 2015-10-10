3730f55796a3c97d7420f7f6576070fef0f5f090
Chase Starr <chasestarr@gmail.com>
2015-10-09 22:43:57 -0400

    Changed port app was listening on

--- app.js
+++ app.js
@@ -48,5 +48,6 @@ app.get('/', function(req,res,next){
 	});
 });
 
-app.listen(3000);
-console.log("server started on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
