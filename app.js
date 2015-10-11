fd1ca8813f865706edd952840e8b28e77ef31bb0
Chase Starr <chasestarr@gmail.com>
2015-10-10 23:00:23 -0400

    added login/signup routes, fixed templating issues

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('views', __dirname + "/views");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var api = 'https://api.deckbrew.com/mtg/cards/';
-var indexTem;
+
 
 function errorHandler(err, req, res, next){
 	console.error(err.message);
@@ -20,13 +20,15 @@ function errorHandler(err, req, res, next){
 	res.status(500);
 	res.render('error_template', {error : err});
 }
-
 app.use(errorHandler);
+
+// Index Page
 app.get('/', function(req,res,next){
 	
 	var name = '?name=' + req.query.name;
 	var searchTerm = api + name;
 	var index = swig.compileFile('./views/index.html');
+  var indexTem;
 
 	request(searchTerm, function (error, response, cards) {
   		if (!error && response.statusCode == 200) {
@@ -38,6 +40,16 @@ app.get('/', function(req,res,next){
 			res.render('index', indexTem);
   		}
 	});
+});
+
+// Login Page
+app.get('/login', function(req,res,next){
+  res.render('login');
+});
+
+// Sign Up Page
+app.get('/signup', function(req,res,next){
+  res.render('signup');
 });
 
 app.listen(process.env.PORT || 3000, function(){
